a5a5cfa0da8c01aead11a0bb332d4638080bf75b
Usuario <Usuario@10.22.148.81>
2019-10-09 13:35:15 +0100

    1st Iteration

---
+++ src/kata2/Histogram.java
@@ -0,0 +1,27 @@
+package kata2;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+class Histogram implements Iterable<Integer>{
+    
+    private final Map<Integer,Integer> counts = new TreeMap<>();
+    Histogram(int... values ) {
+        for (int value : values) {
+            counts.put(value, counts.getOrDefault(value, 0) +1);
+        }
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return counts.keySet().iterator();
+    }
+    
+    public int getCount(int value){
+        return counts.get(value);
+    }
+    
+    
+
+}
